b537600ceea2ea156f95479e75e3dd847925d0e7
Tobias <lokwavetechnologies@gmail.com>
2024-04-19 19:34:30 +0200

    Added API

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,26 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Input from './Input'
 import List from './List'
 
 function App() {
 	const [todo, setTodo] = useState('');
 	const [todos, setTodos] = useState([]);
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		fetch('https://jsonplaceholder.typicode.com/todos')
+			.then((res) => res.json())
+			.then((data) => {
+				const uncompletedTodos = data.filter(todo => !todo.complete)
+					.map(todo => todo.title);
+				setTodos(uncompletedTodos);
+				setLoading(false);				
+			});
+	}, []);
 
 	const addTodo = () => {
+		console.log(todos);
 		if(todo !== ''){
 			setTodos([...todos, todo]);
 			setTodo('');
@@ -23,7 +36,7 @@ function App() {
 		<div className="App">
 			<img className='logo' src="/logo-230116-110523.png" alt="Techover" />
 			<Input setTodo={setTodo} todo={todo} addTodo={addTodo}/>
-			<List todos={todos} complete={complete}/>			
+			<List todos={todos} complete={complete} loading={loading} />				
 		</div>
 	);
 }
